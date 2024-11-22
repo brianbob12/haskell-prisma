67fdb70c0adaed31b1332e18be51a75d61931438
pattokseas <mpattok@seas.upenn.edu>
2024-11-22 12:33:21 -0500

    update raw syntax to treat enum and model fields as one type

--- src/PrismaSchemaRaw.hs
+++ src/PrismaSchemaRaw.hs
@@ -1,33 +1,11 @@
-module PrismaSchemaRaw (
-  Schema,
-  Model,
-  EnumType,
-  DatabaseURL,
-  Field,
-  FieldType,
-  Attribute,
-  Expression,
-  IntExpression,
-  StringExpression,
-  DateTimeExpression
-) where
+module PrismaSchemaRaw where
 
 -- Prisma schema as it exists in the schema.prisma file
 
 data Schema = Schema {
-    databaseUrl :: DatabaseURL,
+    databaseUrl :: String,
     enumTypes :: [EnumType],
     models :: [Model]
-} deriving (Show, Eq)
-
-data Model = Model {
-    modelName :: String,
-    fields :: [Field]
-} deriving (Show, Eq)
-
-data EnumType = EnumDefinition {
-    enumName :: String,
-    values :: [String]
 } deriving (Show, Eq)
 
 data DatabaseURL = 
@@ -35,11 +13,22 @@ data DatabaseURL =
   | EnvironmentVariable String
   deriving (Show, Eq)
 
+data EnumType = EnumDefinition {
+    enumName :: String,
+    values :: [String]
+} deriving (Show, Eq)
+
+data Model = Model {
+    modelName :: String,
+    fields :: [Field]
+} deriving (Show, Eq)
+
 data Field = Field {
     fieldName :: String,
     fieldType :: FieldType,
     attributes :: [Attribute]
  } deriving (Show, Eq)
+
 
 data FieldType = 
   IntField
@@ -53,7 +42,8 @@ data FieldType =
   | ListOf FieldType
   | OptionalField FieldType
   | ModelField String -- Name of the model this field relates to
-  | EnumField String -- Name of the enum type
+--  | EnumField String -- Name of the enum type -- for the raw, should get mapped to Model
+-- since there's no way for the parser to know if it's a model or enum without knowledge of the whole schema
   deriving (Show, Eq)
 
 data Attribute = 
@@ -64,12 +54,6 @@ data Attribute =
   | MapAttribute String  --The database column to use
   | UpdatedAtAttribute
   | IgnoreAttribute
-  deriving (Show, Eq)
-
-data Expression = 
-  IntExpression IntExpression
-  | StringExpression StringExpression
-  | DateTimeExpression DateTimeExpression
   deriving (Show, Eq)
 
 data IntExpression = 
@@ -86,3 +70,10 @@ data StringExpression =
 data DateTimeExpression = 
   NowExpression
   deriving (Show, Eq)
+
+data Expression = 
+  IntExpression IntExpression
+  | StringExpression StringExpression
+  | DateTimeExpression DateTimeExpression
+  deriving (Show, Eq)
+
