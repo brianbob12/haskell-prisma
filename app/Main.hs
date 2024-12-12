6e27eab79359ff8e4a6977bd951defeadf8247e7
pattokseas <mpattok@seas.upenn.edu>
2024-12-12 11:05:32 -0500

    update Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,14 @@
 module Main (main) where
 
-import Lib
+import HaskellPrisma
+import System.Environment (getArgs)
 
 main :: IO ()
-main = someFunc
+main = do
+  args <- getArgs
+  case args of
+    (schema:dir:_) -> (
+      putStrLn ("Generating modules in " ++ dir ++ " from " ++ schema) >> 
+      generateModule schema dir)
+    _ -> putStrLn "Must prove two command-line arguments: the schema.prisma file \
+                \to parse, and the directory to generate modules in"
